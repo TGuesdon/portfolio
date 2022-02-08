533a0d21d54087fac2feecd9ef2fbc863574a574
TGuesdon <thomas.guesdon12@gmail.com>
2022-02-08 17:44:21 +0100

    Factory bg

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FactoryComponent } from './home/factory/factory.component';
+import { LightComponent } from './home/factory/light/light.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FactoryComponent,
+    LightComponent
   ],
   imports: [
     BrowserModule,
