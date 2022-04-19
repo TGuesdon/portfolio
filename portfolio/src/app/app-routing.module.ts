309ddcac3d9176b6c1efdc1303a4e597a77dfd1d
TGuesdon <thomas.guesdon12@gmail.com>
2022-04-19 10:59:00 +0200

    Pages for mobile

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlindtestrapComponent } from './home/factory/pages/blindtestrap/blindtestrap.component';
+import { BuildideasComponent } from './home/factory/pages/buildideas/buildideas.component';
+import { CvComponent } from './home/factory/pages/cv/cv.component';
+import { FinalschoolprojectComponent } from './home/factory/pages/finalschoolproject/finalschoolproject.component';
+import { IdealtekComponent } from './home/factory/pages/idealtek/idealtek.component';
+import { MashuphelperComponent } from './home/factory/pages/mashuphelper/mashuphelper.component';
+import { MenulibComponent } from './home/factory/pages/menulib/menulib.component';
+import { PhotocleanerComponent } from './home/factory/pages/photocleaner/photocleaner.component';
+import { PlanetgeneratorComponent } from './home/factory/pages/planetgenerator/planetgenerator.component';
+import { SpaceconquestComponent } from './home/factory/pages/spaceconquest/spaceconquest.component';
+import { TwitchpartyComponent } from './home/factory/pages/twitchparty/twitchparty.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: 'resume', component: CvComponent },
+  { path: 'twitchparty', component: TwitchpartyComponent },
+  { path: 'idealtek', component: IdealtekComponent },
+  { path: 'photocleaner', component: PhotocleanerComponent },
+  { path: 'spaceconquest', component: SpaceconquestComponent },
+  { path: 'menulib', component: MenulibComponent },
+  { path: 'mashuphelper', component: MashuphelperComponent },
+  { path: 'blindtestrap', component: BlindtestrapComponent },
+  { path: 'buildideas', component: BuildideasComponent },
+  { path: 'planetgenerator', component: PlanetgeneratorComponent },
+  { path: 'finalschoolproject', component: FinalschoolprojectComponent }, 
   { path: '', component: HomeComponent }
 ];
 
